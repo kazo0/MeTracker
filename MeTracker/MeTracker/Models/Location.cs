63d6d82e06e708fc360e4bf8e3647e66175f596e
Steve Bilogan <steve.bilogan@gmail.com>
2020-07-20 21:11:53 -0400

    Add project files.

---
+++ MeTracker/MeTracker/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeTracker.Models
+{
+	public class Location
+	{
+		public int Id { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+
+		public Location(double latitude, double longitude)
+		{
+			Latitude = latitude;
+			Longitude = longitude;
+		}
+
+	}
+}
